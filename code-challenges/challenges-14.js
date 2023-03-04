33ab15df919030f701b720423829e7241ef7c775
Sara-Alkhateeb <sarajamal11399@yahoo.com>
2023-03-04 20:23:25 +0300

    taskes done

---
+++ code-challenges/challenges-14.js
@@ -0,0 +1,143 @@
+'use strict';
+
+// Resource:
+// split: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+// slice: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+// splice: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+// indexOf: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+// lastIndexOf: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf
+// join: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// Using the slice and indexOf/lastIndexOf methods, return the last word in a string
+//
+// Note: consider that the string may have one word but never empty
+//
+// Input: "I did my waiting 12 years of it in Azkaban"
+// Output: "Azkaban"
+//
+// Input: "Impossible"
+// Output: "Impossible"
+//
+
+const LastWord = (str) => {
+  // write your code here
+  const words = str.split(' ');
+  const last = words.slice(-1);
+  return last[0];
+
+}
+
+
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+//
+//  You can try to do the same task using the split method
+//
+//  Input: "you dare use my spells against me, potter"
+//  Output: "potter"
+
+const LastWord_2 = (str) => {
+  // write your code here
+  const words = str.split(' ');
+  return words.pop();
+
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Required:
+//
+// Number 2 was easy right?, ok then using the splice method replace all the "I" with "We", "am" with "are" and "was" with "were"
+//
+// Note: you may use another method for search (indexOf)
+//
+// Input:  "I thought you had a plan"
+// Output: "We thought you had a plan"
+//
+// Input: "I was there 3000 years ago"
+// Output: "We were there 3000 years ago"
+
+// Input: "I am Venom"
+// Output: "We are Venom"
+//
+
+// const replaceWords = (str) => {
+//   // write your code here
+//   const words = str.split(' ');
+//   const iIndex = words.indexOf('I');
+//   if (iIndex !== -1) {
+//     const replacedI = str.replace(/I/gi , 'We');
+//     return replacedI;
+//   }
+//   const wasIndex = words.indexOf('was');
+//   if (wasIndex !== -1) {
+//     const replacedWas = str.replace(/was/gi ,'were');
+//     return replacedWas;
+
+//   }
+
+//   const amIndex = words.indexOf('am');
+//   if (amIndex !== -1) {
+//     const replacedAm = str.replace(/am/gi ,'are');
+//     return replacedAm;
+//   }
+ 
+// }
+const replaceWords = (str) => {
+  const replacedI = str.replace(/I/gi, 'We');
+  const replacedWas = replacedI.replace(/was/gi, 'were');
+  const replacedAm = replacedWas.replace(/am/gi, 'are');
+  return replacedAm;
+}
+
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 04:
+// Optional:
+//
+// Write a function that "joins" the array of words together and puts a comma "," after every five words
+//
+// Input:["move","it","away","from","the","street"] 
+// Output: "move it away from the, street" 
+//
+
+const arrToStr = (arr) => {
+  // write your code here
+  const str = arr.join(' ');
+  return str;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 05:
+// Optional:
+//
+// Simon got a string manipulation question for his interview,
+// it asked him to replace the duplicated letters in a string to an indicator and counter
+// as in the example:
+//
+// Input:"aaaa bbb sdf"
+// Output: "a4 b3 s1d1f1"
+//
+// Input:"door"
+// Output: "d1o2r1"
+//
+// help him by writing the function
+//
+
+const letterCounter = (str) => {
+  // write your code here
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+module.exports = { LastWord, LastWord_2, replaceWords, arrToStr, letterCounter };
