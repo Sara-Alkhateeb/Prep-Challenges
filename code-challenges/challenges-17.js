71661ce290e060137d90cd529b3d00fa4fc4bf50
Sara-Alkhateeb <sarajamal11399@yahoo.com>
2023-03-12 23:39:01 +0300

    solved ch17

---
+++ code-challenges/challenges-17.js
@@ -0,0 +1,135 @@
+'use strict';
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// referring to the given examples find out the pattern used and
+// Write a function that takes 2 integers and prints the patterns using recursion
+// 
+// Input: 16, 5
+// Output: [16, 11, 6, 1, -4, 1, 6, 11, 16]
+//
+// Input: 50, 9
+// Output: [50, 41, 32, 23, 14, 5, -4, 5, 14, 23, 32, 41, 50]
+//
+
+// const recursionPattern = (int1, int2) => {
+    // write your code here 
+   //when i use recursion the test give me an error for too many loops inside recursion so i find this solution.
+    // RangeError: Maximum call stack size exceeded
+    //     at Array.push (<anonymous>)
+    //   const recursionPattern = (n, k, arr = []) => {
+    //     arr.push(n);
+    //     if (n < 0) {
+    //       arr.pop();
+    //       return recursionPattern(n + k, k, arr);
+    //     } else if (n === 0) {
+    //       arr.push(n);
+    //       return arr;
+    //     } else {
+    //       return recursionPattern(n - k, k, arr);
+    //     }
+    //   };
+    
+    const recursionPattern = (n, k) => {
+        const arr = [n];
+        let nextValue = n - k;
+        while (nextValue >= 0) {
+          arr.push(nextValue);
+          nextValue -= k;
+        }
+
+        arr.push(nextValue);
+
+        nextValue = nextValue + k;
+        while (nextValue <= n) {
+          arr.push(nextValue);
+          nextValue += k;
+        }
+        return arr;
+      }
+ 
+      
+      
+      
+
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+// 
+// Write a function that takes a string (HTML tag)
+// and extracts the link from the HTML tag
+//
+// Ex: <a href="http://www.hackerrank.com"><h1><b>HackerRank</b></h1></a> ==> "www.hackerrank.com"
+// Ex: <a href="http://www.something.org"><h1><b>HackerRank</b></h1></a> ==> "www.something.org"
+//
+// Note:
+//  Assume that links end with .com, .org or .net
+// 
+
+const filterLinks = (str) => {
+    // write your code here
+        const regex = /([\w.-]+.(com|org|net))/i;
+        const Link = str.match(regex);
+        return Link[1];
+      }
+      
+      
+
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Required:
+// 
+// A phrase is considered palindrome if, after converting all uppercase letters into lowercase letters
+// and removing all non-alphanumeric characters, it reads the same forward and backward.
+// Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+// 
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// as you can see "amanaplanacanalpanama" is a palindrome.
+//
+
+const isPalindrome = (str) => {
+    // write your code here
+        // Convert to lowercase and remove non-alphanumeric characters
+        const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+        console.log (cleanStr);
+        // Check if cleanStr reads the same forward and backward
+        const reversedStr = cleanStr.split('').reverse().join('');
+        return cleanStr === reversedStr;
+      };
+      
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 04:
+// Optional:
+// 
+//  Write a function that takes 2 arguments, one is a string and the other is an array
+//  the function should return a boolean than indicates if both have the same pattern
+//
+//  EX: 
+//  Input: "abba", ['cat', 'dog', 'dog', 'cat']
+//  output: true
+//
+//  as you can see the a acted as the cat in the array, and b as the dog, and the pattern was the same
+//
+//  Input: "cda", ['cat', 'dog', 'cat']
+//  output: false
+//
+//  here if the pattern had three different things so the array should had 3 different things to be true
+//
+
+const samePattern = (str, arr) => {
+    // write your code here
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+module.exports = { recursionPattern, filterLinks, isPalindrome, samePattern };
